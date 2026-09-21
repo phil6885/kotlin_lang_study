9aca8baafec3cff48e3fba3439dff7962a2b7cbe
hongphil <phil6885@gmail.com>
2026-09-21 15:54:22 +0900

    Convention(operator fun) example

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
+data class Point(val x: Int, val y: Int) {
+//    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+}
+
+operator fun Point.plus(p: Point) = Point(x + p.x, y + p.y)
+
+
 fun main(args: Array<String>) {
-    println("Hello Hong phil")
+    val p1 = Point(10, 20)
+    val p2 = Point(30, 40)
+    println(p1 + p2)
 }
