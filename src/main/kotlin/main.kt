c4dea538449f867c5bc7ed4ff67eb609c5d82856
hongphil <phil6885@gmail.com>
2026-09-21 16:09:35 +0900

    add 2.0 * p <--> p * 2.0

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,10 +3,13 @@ data class Point(val x: Int, val y: Int) {
 }
 
 operator fun Point.plus(p: Point) = Point(x + p.x, y + p.y)
-
+operator fun Point.times(scale: Double) = Point((x * scale).toInt(), (y * scale).toInt())
+operator fun Double.times(p: Point) = p * this
 
 fun main(args: Array<String>) {
     val p1 = Point(10, 20)
     val p2 = Point(30, 40)
     println(p1 + p2)
+    println(p1 * 2.0)
+    println(2.0 * p1)
 }
