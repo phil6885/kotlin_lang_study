f5169311aab0fd72df49269b5f9017aa54dcdb76
hongphil <phil6885@gmail.com>
2026-09-22 17:28:35 +0900

    override equals method

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,21 @@
 data class Point(val x: Int, val y: Int) {
-//    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+    //    operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+    override fun equals(other: Any?): Boolean {
+        println("equals function was called")
+        if (this === other) return true
+        if (other !is Point) return false
+        return x == other.x && y == other.y
+    }
 }
-
+//data class has already overridden it, so extension function does not work
+//fun Point.equals(other: Any?): Boolean {
+//    if(other === this) return true
+//    if(other !is Point) return false
+//    return x ==other.x && y == other.y
+//}
+operator fun Double.times(p: Point) = p * this
 operator fun Point.plus(p: Point) = Point(x + p.x, y + p.y)
 operator fun Point.times(scale: Double) = Point((x * scale).toInt(), (y * scale).toInt())
-operator fun Double.times(p: Point) = p * this
 
 fun main(args: Array<String>) {
     val p1 = Point(10, 20)
@@ -12,6 +23,8 @@ fun main(args: Array<String>) {
     println(p1 + p2)
     println(p1 * 2.0)
     println(2.0 * p1)
+    println(p1 == p2)
+    println(p1 != p2)
 
     p2 += p1
     println(p2)
